d9fa824ed8c6e713b3d1e42c1b6917938def1a3d
DylanM05 <97370609+DylanM05@users.noreply.github.com>
2024-04-12 00:26:30 -0400

    preparing for deployment

--- server/server.js
+++ server/server.js
@@ -4,9 +4,18 @@ const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
 const populateRoutes = require('./Routes/populateRoute');
+const PORT = process.env.PORT || 3001;
+const allowedOrigins = ['http://localhost:3000', 'https://dylanmcmullen.vercel.app'];
 
 app.use(cors({
-  origin: 'http://localhost:3000'
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
 }));
 app.use(express.json());
 
@@ -18,6 +27,7 @@ mongoose
   .then(() => console.log("Successfully connected to MongoDB"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
-app.listen(3001, () => {
+
+app.listen(PORT, () => {
   console.log('Server is running on http://localhost:3001');
 });
