710556d874b6ba772cc53abdd8226e0089ab8f70
DylanM05 <97370609+DylanM05@users.noreply.github.com>
2024-04-12 02:25:06 -0400

    Added a loading page for resume

--- client/src/Components/Resume.js
+++ client/src/Components/Resume.js
@@ -14,7 +14,7 @@ const Resume = () => {
   const [hardSkills, setHardSkills] = useState([]);
   const [softSkills, setSoftSkills] = useState([]);
   const [volunteer, setVolunteer] = useState([]);
-
+  const [isLoading, setIsLoading] = useState(true);
 
 
   useEffect(() => {
@@ -23,15 +23,39 @@ const Resume = () => {
     });
   
     api.get('/api/populate')
-      .then(response => {
-        setEducation(response.data.education);
-        setExperience(response.data.experience);
-        setHardSkills(response.data.hardskills);
-        setSoftSkills(response.data.softskills);
-        setVolunteer(response.data.volunteer);
-      })
-      .catch(error => console.error('Error fetching data:', error));
-  }, []);
+    .then(response => {
+      setEducation(response.data.education);
+      setExperience(response.data.experience);
+      setHardSkills(response.data.hardskills);
+      setSoftSkills(response.data.softskills);
+      setVolunteer(response.data.volunteer);
+      setIsLoading(false); // Set loading to false after data is fetched
+    })
+    .catch(error => {
+      console.error('Error fetching data:', error);
+      setIsLoading(false); // Also set loading to false if there's an error
+    });
+}, []);
+
+
+  // If the data is loading, render a loading spinner
+if (isLoading) {
+  return (
+    <div className={`resume container-fluid ${theme === 'dark' ? 'dark-mode' : ''}`}> 
+    <div className="spinner-page">
+      <h1>Loading...</h1>
+      <h2>The backend is hosted on render.com and takes a minute to boot up.</h2>
+    <div className="spinner">
+      <div className="rect1"></div>
+      <div className="rect2"></div>
+      <div className="rect3"></div>
+      <div className="rect4"></div>
+      <div className="rect5"></div>
+    </div>
+  </div>
+  </div>
+  );
+}
 
   return (
     <div className={`resume container-fluid ${theme === 'dark' ? 'dark-mode' : ''}`}> 
