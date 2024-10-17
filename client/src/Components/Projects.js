1b6a0b7fd2a43f7b5cc1e874555f2a7cdb770e94
Dylan McMullen <dmcmull1@my.centennialcollege.ca>
2024-10-17 17:47:47 -0400

    overhaul on projects page

--- client/src/Components/Projects.js
+++ client/src/Components/Projects.js
@@ -1,21 +1,18 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { Card, Modal, Button, Carousel } from 'react-bootstrap';
-import { DiAndroid, } from "react-icons/di";
-import { SiPython, SiPreact, SiBootstrap, SiMongodb, SiPortainer } from "react-icons/si";
-import { FaNodeJs } from "react-icons/fa";
+import React, { useState, useEffect } from 'react';
+import { Card, Modal, Button, Carousel, Container } from 'react-bootstrap';
+import { DiAndroid } from "react-icons/di";
+import { SiPython, SiPreact, SiBootstrap, SiMongodb } from "react-icons/si";
+import { FaNodeJs, FaGithub } from "react-icons/fa";
 import { BiSolidFileCss } from "react-icons/bi";
 import { TbFileTypeXml, TbBrandKotlin } from "react-icons/tb";
 
-import { FaGithub } from "react-icons/fa";
-import './Styles/Projects.css';
 import Header from './Header';
 import Footer from './Footer';
 
+import './Styles/Projects.css';
 import Ethgas from '../Images/ethgas.png';
 import Maticgas from '../Images/maticgas.png';
 import androidapp1 from '../Images/androidapp1.png';
-import androidapp2 from '../Images/androidapp2.png';
 import portfolioapp1 from '../Images/portfolioapp1.png';
 import portfolioapp2 from '../Images/portfolioapp2.png';
 import ezdeals1 from '../Images/ezdeals1.png';
@@ -23,16 +20,10 @@ import ezdeals2 from '../Images/ezdeals2.png';
 import portainer from '../Images/portainer.png';
 import webscrape from '../Images/webscrape.png';
 
-function Projects() {
-  const navigate = useNavigate();
-
-  const goToDiscWidget = () => {
-    navigate('/discbot');
-  };
-
+const Projects = () => {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
   const [show, setShow] = useState(false);
   const [modalImages, setModalImages] = useState([]);
-  const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
   const handleClose = () => setShow(false);
   const handleShow = (images, index) => {
@@ -41,23 +32,39 @@ function Projects() {
     setShow(true);
   };
 
+  useEffect(() => {
+    const controls = document.querySelectorAll('.carousel-control-prev, .carousel-control-next');
+
+    const handleBlur = (event) => {
+      event.target.blur();
+    };
+
+    controls.forEach(control => {
+      control.addEventListener('click', handleBlur);
+    });
+
+    return () => {
+      controls.forEach(control => {
+        control.removeEventListener('click', handleBlur);
+      });
+    };
+  }, []);
+
   const projects = [
     {
       title: 'ETH / MATIC gas Discord Bot',
       subtitle: <div>Language: <SiPython /> Python </div>,
-      description: 'This was made during the Crypto craze of 2022. I made this discord bot that retrieves the gas price of ETH and MATIC using Etherscan or polygonscan',
+      description: 'I made this in 2022, when Cryptocurrency was all the hype. This is a discord bot that retrieves the gas price of ETH and MATIC using Etherscan or polygonscan',
       imagedisc:'Click the images to view them in full size',
       images: [Maticgas, Ethgas],
       githubLink: 'https://github.com/DylanM05/Gas-bot',
       buttonText: 'GitHub',
-      buttonText: 'Try it out!',
-      onClick: goToDiscWidget
     },
     {
       title: 'Student - Teacher Management App',
       subtitle: <div>Language: <DiAndroid /> <TbBrandKotlin/>Kotlin <TbFileTypeXml/>XML</div> ,
       description: 'This is a simple Android app that allows a teacher to register, add and manage students, tests, scores that are stored in the local Database',
-      images: [androidapp1, androidapp2],
+      images: [androidapp1],
       githubLink: 'https://github.com/DylanM05/Android-Student-Teacher-Management-App',
       buttonText: 'GitHub'
     },
@@ -82,7 +89,7 @@ function Projects() {
     {
       title: 'Indeed Webscraper',
       subtitle: <div>Language: <SiPython /> Python </div>,
-      description: 'This is a webscraper that I made using Python. It scrapes the Indeed website for job postings and stores them in a CSV file.',
+      description: 'This is a webscraper that I made using Python. It scrapes the Indeed website for job postings with specified keywords and stores them in a CSV file.',
       images: [webscrape],
       githubLink: 'https://github.com/DylanM05/IndeedWebScraper',
       buttonText: 'GitHub'
@@ -90,69 +97,75 @@ function Projects() {
     {
       title: 'Home Media & Cloud server',
       subtitle: <div>Running: Ubuntu Server and seperating everything into Docker containers</div>,
-      description: 'This is a server that I made using Ubuntu Server. It runs a Plex server, Nextcloud server, and a few other services. I have everything separated into Docker containers for easy management.',
+      description: 'This is a server that I made using Ubuntu Server. It runs a Plex server, Nextcloud server, and a few other services. I have everything separated into Docker containers for easy management. I have also created an automated backup script that runs every week, and replaces the old backup with the new one.',
       images: [portainer],
     }
   ];
 
   return (
-    <div>
+    <div className="App" style={{ backgroundColor: 'var(--Resume-background)', color: 'var(--Resume-text)' }}>
       <Header />
-      <h1 className='Title'>My Projects</h1>
-      <div className='card-container'>
-        <div className='parent-div'>
-          <div className='projects-grid'>
-            {projects.map((project, index) => (
-              <Card key={index} className="project">
-                <Card.Body>
-                  <Card.Title className="card-title">{project.title}</Card.Title>
-                  <Card.Subtitle className="card-subtitle">
-                    {project.subtitle}
-                  </Card.Subtitle>
-                  <Card.Text className="card-text">{project.description}</Card.Text>
-      <div className='Card-images'>
-        {project.images.length === 1 ? (
-          <Card.Img className='Card-Img' src={project.images[0]} alt="Project" onClick={() => handleShow(project.images, 0)} />
-        ) : (
-          <div className="carousel-container">
-            <Carousel interval={null} className='carousel'>
-              {project.images.map((image, i) => (
-                <Carousel.Item key={i} onClick={() => handleShow(project.images, i)}>
-  <img className="d-block w-100" src={image} alt={`Slide ${i}`} />
-</Carousel.Item>
-              ))}
-            </Carousel>
-          </div>
-        )}
-      </div>
-      <Card.Text className="img-disc">Click the images to view them in full size</Card.Text>
-      <div className='button-container'>
-        <a href={project.githubLink} target="_blank" rel="noopener noreferrer">
-          <button className='card-button' variant="outline-dark"><FaGithub /> GitHub</button>
-        </a>
-        {project.onClick && (
-          <button className='card-button' variant="outline-dark" onClick={project.onClick}>{project.buttonText}</button>
-        )}
-        {project.websiteLink && (
-          <a href={project.websiteLink} target="_blank" rel="noopener noreferrer">
-            <button className='card-button' variant="outline-dark">{project.websiteButtonText}</button>
-          </a>
-        )}
-      </div>
-    </Card.Body>
-  </Card>
-))}
-
-          </div>
+      <Container className="content">
+        <h1 className='text-center display-4 my-4' style={{color: 'var(--Resume-text)' }}>My Projects</h1>
+        <Carousel interval={null}>
+          {projects.map((project, index) => (
+            <Carousel.Item key={index} className="carousel-item">
+<Card className="project" style={{ color: 'var(--Resume-text)', boxShadow: `0 4px 20px var(--shadow-color)`, border: 'none', height: '1500px' }}>
+  <Card.Body>
+    <Card.Title className="card-title">{project.title}</Card.Title>
+    <Card.Subtitle className="card-subtitle mb-2" style={{ color: 'var(--Resume-text)' }}>
+      {project.subtitle}
+    </Card.Subtitle>
+    <Card.Text className="card-text" style={{ color: 'var(--Resume-text)' }}>{project.description}</Card.Text>
+    <div className='d-flex justify-content-center my-3'>
+      {project.images.length === 1 ? (
+        <Card.Img className='img-fluid' src={project.images[0]} alt="Project" onClick={() => handleShow(project.images, 0)} style={{ objectFit: 'cover'}} />
+      ) : (
+        <div className="w-100">
+          <Carousel 
+            indicators={true} 
+            controls={false} 
+            interval={7000} 
+            className="image-carousel" 
+            style={{ backgroundColor: 'var(--indicator-color)' }}
+          >
+            {project.images.map((image, i) => (
+              <Carousel.Item key={i} onClick={() => handleShow(project.images, i)}>
+                <img className="d-block w-100 image-carousel" src={image} alt={`Slide ${i}`} />
+              </Carousel.Item>
+            ))}
+          </Carousel>
         </div>
-      </div>
-      <Modal show={show} onHide={handleClose} dialogClassName="modal-dialog" contentClassName="modal-content">
-        <Modal.Body style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', padding: 0, position: 'relative' }}>
-          <img src={modalImages[currentImageIndex]} alt="Zoomed project" style={{ maxWidth: '90vw', maxHeight: '90vh' }} />
-          <Button className="close-button" id='close-button' onClick={handleClose}>Close</Button>
+      )}
+    </div>
+    <Card.Text className="text-center" style={{ color: 'var(--Resume-text)' }}>Click the images to view them in full size</Card.Text>
+    <div className='d-flex justify-content-center gap-2'>
+      <a href={project.githubLink} target="_blank" rel="noopener noreferrer">
+        <Button variant="outline-dark" style={{ backgroundColor: 'var(--Resume-button)', color: 'var(--Resume-button-text)' }}><FaGithub /> GitHub</Button>
+      </a>
+      {project.websiteLink && (
+        <a href={project.websiteLink} target="_blank" rel="noopener noreferrer">
+          <Button variant="outline-dark" style={{ backgroundColor: 'var(--Resume-button)', color: 'var(--Resume-button-text)' }}>{project.websiteButtonText}</Button>
+        </a>
+      )}
+    </div>
+  </Card.Body>
+</Card>
+
+
+            </Carousel.Item>
+          ))}
+        </Carousel>
+      </Container>
+      <Footer />
+      <Modal show={show} onHide={handleClose} dialogClassName="modal-dialog" contentClassName="bg-transparent border-0">
+        <Modal.Body className="d-flex justify-content-center align-items-center" style={{ height: '100vh', padding: 0, position: 'relative' }}>
+          <img src={modalImages[currentImageIndex]} alt="Zoomed project" className="img-fluid" style={{ maxWidth: '90vw', maxHeight: '90vh' }} />
+          <button type="button" className="btn btn-danger" style={{ position: 'absolute', top: '10px', right: '-25vw' }} aria-label="Close" onClick={handleClose}>
+            Close
+          </button>
         </Modal.Body>
       </Modal>
-      <Footer />
     </div>
   );
 }
